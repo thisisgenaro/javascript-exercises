0be142b9731c4404218d723d69be220008e88616
Genaro Hernandez <genaro.hernandez@outlook.com>
2023-09-01 00:06:14 -0400

    js exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
 
+    let output = "";
+    if(num === 0)
+    {}
+    else if(num < 0 || isNaN(num))
+    {
+        output = "ERROR"
+    }
+    else{
+        for(let i=0 ;i < Math.abs(num); i++)
+    {
+        output += str;
+    }
+    }
+    return output
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+    let outArr = [];
+    if(Array.isArray(arr))
+    {
+        for(let j = 0; j < arr.length; j++)
+        {
+            let inside = false;
+            for(let i = 0; i < args.length; i++)
+            {
+                if(arr[j] === args[i])
+                {
+                    inside = true;
+                }
+            }
+            if(!inside)
+            {
+                outArr.push(arr[j]);
+            }
+        }
+    }
 
+    return outArr;
 };
 
 // Do not edit below this line
