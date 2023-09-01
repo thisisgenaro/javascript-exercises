a4a7a361ff5add78b24dc77213808d1a7ffce539
Genaro Hernandez <genaro.hernandez@outlook.com>
2023-09-01 11:09:38 -0400

    exercises done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1,num2) {
+    if(Number.isInteger(num1) && Number.isInteger(num2) && Math.min(num1,num2) >= 0)
+    {
+        let result = Math.min(num1,num2);
+        for(let i = result + 1; i <= (Math.max(num1,num2)); i++)
+        {
+            result += i;
+        }
+        return result;
+    }
+    else
+    {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let ans = false;
+    /*return year % 4 === 0 ? (year % 100 === 0 ? (year % 400 === 0) : false) : false;*/
+    if(Number.isInteger(year))
+    {
+        if(year % 4 === 0)
+        {
+            if(year % 100 === 0 && year % 400 != 0)
+            {
+                return ans;
+            }
+            else
+            {
+                ans = true;
+            }
+        }
+    }
+    return ans;
 };
 
 // Do not edit below this line
